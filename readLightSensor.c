236020124ff7d5c980a0cc55768adb3a2dde16bc
Cyphrus <wamaynes@gmail.com>
2014-01-31 18:41:46 -0500

    such following, much line, very good
    We have a line follower that can complete the easy and normal courses.

---
+++ readLightSensor.c
@@ -0,0 +1,49 @@
+#pragma config(Sensor, S3,     lightSensor,         sensorLightActive)
+
+task main()
+{
+	wait1Msec(50);                        // The program waits 50 milliseconds to initialize the light sensor.
+
+	int onEdge = 45;
+	int allLine = 35; // On the line
+	int allOff = 65;  // Off the line
+	while(true)                           // Infinite loop
+	{
+		int lightVal = SensorValue[lightSensor];
+		//int prop = onEdge - SensorValue[lightSensor];
+		if (lightVal <= 35) {
+			motor[motorB] = -20;
+			motor[motorC] = 5;
+		}
+		else if(lightVal < 38) {
+			motor[motorB] = -5;
+			motor[motorC] = 10;
+		}
+		else if (lightVal < 40) {
+			motor[motorB] = 5;
+			motor[motorC] = 40;
+		}
+		else if (lightVal < 45) {
+			motor[motorB] = 28;
+			motor[motorC] = 30;
+		}
+		else if (lightVal == 45) {
+			motor[motorB] = 20;
+			motor[motorC] = 20;
+		}
+		else if (lightVal < 55) {
+			motor[motorB] = 30;
+			motor[motorC] = 28;
+		}
+		else if (lightVal < 65) {
+			motor[motorB] = 40;
+			motor[motorC] = 0;
+		}
+		else {
+			motor[motorB] = 20;
+			motor[motorC] = -30;
+		}
+		writeDebugStream("LightSensor is : %d\n", lightVal);
+		wait1Msec(20);
+	}
+}
