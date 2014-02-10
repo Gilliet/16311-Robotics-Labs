687557d6d60bb06bf51d9e45ffb68adbacfe1d59
Rauhit Ashar <asharrauhit@gmail.com>
2014-02-10 15:44:32 -0500

    Fixed some of the trajectory following.

--- Picture.c
+++ Picture.c
@@ -12,22 +12,12 @@ float sixx (float t) {
 	return 0.1 * (2.0 * (pow(cos(t / 12.0),3.0)) + 1) * (sin(t / 4.0));
 }
 float sixy (float t) {
-	return 0.1 * (cos(t / 4.0)) * (1 - 2 * (pow(sin(t / 4.0),4.0))) - 0.1;
+	return 0.1 * (cos(t / 4.0)) * (1 - 2 * (pow(sin(t / 4.0),4.0)));
 }
 
 int vtop (float v) {
 	return (int)((102.313 * v) + 0.000179047);
 }
-
-float fivex (float t) {
-	return (0.1*(-2.0*(pow(cos(t/5.0),2.0))-(sin(t/10.0))+1.0)*(sin(t/5.0)));
-}
-
-
-float fivey (float t) {
-	return (0.1*(cos(t/5.0))*(-2.0*(pow(cos(t/5.0),3.0))-(sin(t/10.0))+1.0)) + 0.1; //+0.1 is for offset
-}
-
 
 float crazyx (float t) {
 	return 0.02 * (5.0 * cos(9.0 * t / 20.0) - 4 * cos(t / 4.0));
@@ -47,25 +37,37 @@ float lotusy (float t) {
 	return .5 * r(t) * sin(t);
 }
 
+float lambdax (float t) {
+	if (t < 3) {
+		return cos(t) * .02 - .02;
+		} else if (t < 5){
+		return -.10;
+		} else if (t < 7) {
+		return -.03;
+		} else {
+		return -.02;
+	}
+}
+
+float lambday (float t) {
+	if (t < 3) {
+		return sin(t) * .02;
+		} else if (t < 5){
+		return -.10;
+		} else if (t < 7) {
+		return -.03;
+		} else {
+		return -.02;
+	}
+}
 
 float ninex (float t) {
-	if (t < 10) {
-		return t * 1.5;
-		} else if (10 < t < 20) {
-		return (t - 10);
-		} else {
-		return 10;
-	}
+	return cos(t) * .04 - .0956;
 }
 float niney (float t) {
-	if (t < 5) {
-		return 0;
-		} else  if (5 < t < 15) {
-		return -(t-5) * .5;
-		} else {
-		return 5;
-	}
+	return sin(t) * .04 - .0947;
 }
+
 
 
 task main()
@@ -76,7 +78,7 @@ task main()
 	float lastt = time10[T2];
 	float des_x;
 	float des_y;
-	float K = .02; // GAIN
+	float K = .1; // GAIN
 	float D = -5;
 	float x = 0.0;
 	float y = 0;
@@ -87,7 +89,7 @@ task main()
 	int renc = nMotorEncoder[motorC];
 	float lastex = 0;
 	float lastey = 0;
-	while (t < 63.0)
+	while (t < 3.5)
 	{
 		// DEAD RECKONING
 		int newl = nMotorEncoder[motorB];
@@ -108,8 +110,8 @@ task main()
 		wait1Msec(10);
 		t += (time10[T2] - lastt) / 100.0;
 		lastt = time10[T2];
-		des_x = trajx(t) * 100;
-		des_y = trajy(t) * 100;
+		des_x = lambdax(t) * 100;
+		des_y = lambday(t) * 100;
 		//		writeDebugStream("%f",des_y);
 		// END TRAJECTORY
 
@@ -123,7 +125,7 @@ task main()
 		lastex = dx;
 		lastey = dy;
 
-		writeDebugStream("x:%f,y:%f\n",sixx(0),sixy(0));
+		writeDebugStream("x:%f,y:%f\n",dx,dy);
 		float outV = cos(pose) * (dx + vx) + sin(pose) * (dy + vy);
 		float outw = cos(pose) * (dy + vy) - sin(pose) * (dx + vx);
 		float outvl = outV + WB * outw / 2;
@@ -132,6 +134,76 @@ task main()
 		int RM = vtop(outvr);
 		motor[motorC] = LM;
 		motor[motorB] = RM;
+	}
+	while (t < 4.5) {
+		t += (time10[T2] - lastt) / 100.0;
+		lastt = time10[T2];
+		motor[motorC] = -7;
+		motor[motorB] = -7;
+	}
+	while (t < 5.2) {
+		t += (time10[T2] - lastt) / 100.0;
+		lastt = time10[T2];
+		motor[motorB] = -5;
+		motor[motorC] = 5;
+	}
+	while (t < 6.1) {
+		t += (time10[T2] - lastt) / 100.0;
+		lastt = time10[T2];
+		motor[motorC] = 17;
+		motor[motorB] = 17;
+	}
+	while (t < 14.7)
+	{
+		// DEAD RECKONING
+		int newl = nMotorEncoder[motorB];
+		int newr = nMotorEncoder[motorC];
+		vl = RAD * (newl - lenc) * (PI / 180.0);
+		vr = RAD * (newr - renc) * (PI / 180.0);
+		renc = newr;
+		lenc = newl;
+		float V = (vr + vl) / 2.0;
+		float w = (vr - vl) / WB;
+		float theta = pose + w / 2.0;
+		x = x + (V * cos(theta));
+		y = y + (V * sin(theta));
+		pose = pose + w;
+		// END DEAD RECKONING
+
+		// TRAJECTORY
+		wait1Msec(10);
+		t += (time10[T2] - lastt) / 100.0;
+		lastt = time10[T2];
+		des_x = ninex(t) * 100;
+		des_y = niney(t) * 100;
+		//		writeDebugStream("%f",des_y);
+		// END TRAJECTORY
+
+		float dx = K * (des_x - x); // ERROR
+		float dy = K * (des_y - y); // PROPORTIONAL GAIN
+		float vx = D * ((dx - lastex) / (t - lastt)); // DIFFERENTIAL GAIN
+		float vy = D * ((dy - lastey) / (t - lastt));
+		nxtDisplayTextLine(2,"%f,", x);
+		nxtDisplayTextLine(3,"%f",y);
+		nxtDisplayTextLine(4,"%f",(pose*180.0)/PI);
+		lastex = dx;
+		lastey = dy;
+
+		writeDebugStream("x:%f,y:%f\n",dx,dy);
+		float outV = cos(pose) * (dx + vx) + sin(pose) * (dy + vy);
+		float outw = cos(pose) * (dy + vy) - sin(pose) * (dx + vx);
+		float outvl = outV + WB * outw / 2;
+		float outvr = outV - WB * outw / 2;
+		int LM = vtop(outvl);
+		int RM = vtop(outvr);
+		motor[motorC] = LM;
+		motor[motorB] = RM;
+	}
+		while (t < 15.6) {
+		t += (time10[T2] - lastt) / 100.0;
+		lastt = time10[T2];
+		motor[motorC] = 17;
+		motor[motorB] = 17;
 	}
 	motor[motorC] = 0;
 	motor[motorB] = 0;
