09298075b3969913c08b36e5d5d62c52fbd7c01b
Rauhit Ashar <asharrauhit@gmail.com>
2014-02-17 15:17:43 -0500

    DFS still not working. Possible causes could be max recursion depth.

--- DFS.c
+++ DFS.c
@@ -11,7 +11,7 @@ typedef struct graphNode{
 // Path struct. It's good stuff.
 typedef struct path {
 	int nElems;
-	float coords[14];
+	float coords[16];
 } path;
 
 // Small helper.
@@ -24,9 +24,9 @@ int alreadyVisited (node* visited, int n, node* next) {
 
 int DFS (path* p, node* currNode, node* endNode, node* visited, int nVisited) {
 	// Mark the current node as visited.
-  writeDebugStream("visiting: (%f2,%f2)\n",currNode->x,currNode->y);
+	writeDebugStream("visiting: (%f2,%f2)\n",currNode->x,currNode->y);
 	if (p == NULL) writeDebugStream("p is null?! \n");
-  visited[nVisited] = *currNode;
+	visited[nVisited] = *currNode;
 	nVisited ++;
 	// Number of elements in the path coming into this node
 	int nPath = p->nElems;
@@ -35,25 +35,29 @@ int DFS (path* p, node* currNode, node* endNode, node* visited, int nVisited) {
 		p->coords[2*nPath] = currNode->x;
 		p->coords[2*nPath+1] = currNode->y;
 		p->nElems++;
+		writeDebugStream("Adding to the path: (%f2,%f2)\n",currNode->x,currNode->y);
 		return 1;
-	} else {
+		} else {
 		// We're not at the destination, so lets see if we are part of the path
-		p->coords[2*nPath] = currNode->x;
-		p->coords[2*nPath + 1] = currNode->y;
 		int i = 0;
 		// Check through each of our neighbors
 		for (int i = 0; i < 5; i++) {
 			// To make sure we don't infinitely loop, check that we haven't already
 			// visited the node, especially important since the graph has undirected
-		  // edges.
-		writeDebugStream("nVisited: %d \n",nVisited);
+			// edges.
+			//	writeDebugStream("nVisited: %d \n",nVisited);
 			if(!alreadyVisited(visited, nVisited, currNode->neighbors[i])) {
 				// Reset the location in the path after each unsuccessful branch we explore
-				p->nElems = nPath + 1;
+				//				p->nElems = nPath + 1;
 				int goodPath = DFS(p, currNode->neighbors[i], endNode, visited, nVisited);
 				// We have found a good path, and that means that all of the coordinates we
 				// need to go through have been added to the path in order.
-				if(goodPath) return 1;
+				if(goodPath) {
+					p->coords[2*nPath] = currNode->x;
+					p->coords[2*nPath + 1] = currNode->y;
+					p->nElems++;
+					return 1;
+				}
 				//
 				// Insert cleaning loop here if necessary to remove extra elements from
 				// the path
@@ -63,6 +67,7 @@ int DFS (path* p, node* currNode, node* endNode, node* visited, int nVisited) {
 		}
 		// If we're here, we didn't find it by checking all of our neighbors, so we
 		// signal that to the rest of the code.
+		writeDebugStream("backtracking out of: (%f2,%f2)\n",currNode->x,currNode->y);
 		return 0;
 		// Each point in the DFS keeps track of the variables that it needs for
 		// successful recursive calls, so we shouldn't have to reset anything.
--- pathFinder.c
+++ pathFinder.c
@@ -8,12 +8,12 @@ task main()
 {
 	float startx = 0.0;
 	float starty = 0.0;
-	float goalx = 0.1;
-	float goaly = 0.5;
-  writeDebugStream("starting!\n");
+	float goalx = .3048;//1.676;
+	float goaly = .9144;//0.6096;
+	writeDebugStream("starting!\n");
 	robotModel rob;
 	init(&rob,startx,starty,0.0);
-  writeDebugStream("init successful.\n");
+	writeDebugStream("init successful.\n");
 
 
 	//wow sorry
@@ -23,12 +23,12 @@ task main()
 
 
 
-		writeDebugStream("making path.\n");
-  node graph[NUM_WAYPOINTS];
+	writeDebugStream("making path.\n");
+	node graph[NUM_WAYPOINTS];
 	//bottom row
-  graph[0].x = 0.1524; graph[0].y = 0.1524;
+	graph[0].x = 0.1524; graph[0].y = 0.1524;
 	graph[1].x = 0.762;	 graph[1].y = 0.1524;
-  graph[2].x = 0.9144; graph[2].y = 0.1524;
+	graph[2].x = 0.9144; graph[2].y = 0.1524;
 	graph[3].x = 2.134;  graph[3].y = 0.1524;
 	//top row
 	graph[4].x = 0.3048; graph[4].y = 0.9144;
@@ -41,18 +41,18 @@ task main()
 	graph[11].x = 1.676; graph[11].y = 0.1524;
 	//set up neighbors. do we need to make the remaining ones NULL?
 	graph[0].neighbors[0] = &(graph[1]);
-		graph[0].neighbors[1] = NULL; graph[0].neighbors[2] = NULL;	graph[0].neighbors[3] = NULL;		graph[0].neighbors[4] = NULL;
+	graph[0].neighbors[1] = NULL; graph[0].neighbors[2] = NULL;	graph[0].neighbors[3] = NULL;		graph[0].neighbors[4] = NULL;
 	graph[1].neighbors[0] = &(graph[0]);
 	graph[1].neighbors[1] = &(graph[2]);
-		graph[1].neighbors[2] = NULL;	graph[1].neighbors[3] = NULL;		graph[1].neighbors[4] = NULL;
+	graph[1].neighbors[2] = NULL;	graph[1].neighbors[3] = NULL;		graph[1].neighbors[4] = NULL;
 	graph[2].neighbors[0] = &(graph[1]);
 	graph[2].neighbors[1] = &(graph[9]);
 	graph[2].neighbors[2] = &(graph[10]);
-		graph[2].neighbors[3] = NULL;		graph[2].neighbors[4] = NULL;
+	graph[2].neighbors[3] = NULL;		graph[2].neighbors[4] = NULL;
 	graph[3].neighbors[0] = &(graph[7]);
 	graph[3].neighbors[1] = &(graph[8]);
 	graph[3].neighbors[2] = &(graph[11]);
-graph[3].neighbors[3] = NULL;		graph[3].neighbors[4] = NULL;
+	graph[3].neighbors[3] = NULL;		graph[3].neighbors[4] = NULL;
 
 
 	graph[4].neighbors[0] = &(graph[5]);
@@ -66,7 +66,7 @@ graph[3].neighbors[3] = NULL;		graph[3].neighbors[4] = NULL;
 	graph[7].neighbors[0] = &(graph[6]);
 	graph[7].neighbors[1] = &(graph[9]);
 	graph[7].neighbors[2] = &(graph[3]);
-		graph[7].neighbors[3] = NULL; graph[7].neighbors[4] = NULL;
+	graph[7].neighbors[3] = NULL; graph[7].neighbors[4] = NULL;
 	graph[8].neighbors[0] = &(graph[3]);
 	graph[8].neighbors[1] = NULL;		graph[8].neighbors[2] = NULL; graph[8].neighbors[3] = NULL;		graph[8].neighbors[4] = NULL;
 	graph[9].neighbors[0] = &(graph[2]);
@@ -87,57 +87,58 @@ graph[3].neighbors[3] = NULL;		graph[3].neighbors[4] = NULL;
 	goal.y = goaly;
 	st.neighbors[0] = NULL;
 	goal.neighbors[0] = NULL;
-writeDebugStream("set up neighbors, except for start and goal.\n");
+	writeDebugStream("set up neighbors, except for start and goal.\n");
 
 	//now add start and end- go through waypoints, finding closest valid one.
 	//connect the start and end to those points.
 
- for(int i = 0; i < 1; i++){
+	for(int i = 0; i < NUM_WAYPOINTS; i++){
 		if (st.neighbors[0] == NULL){
 			writeDebugStream("set st's initial neighbor. \n");
 			st.neighbors[0] = &graph[i];
 			graph[i].neighbors[3] = &st;
 			writeDebugStream("st.x: %f2 st.y: %f2 stn.x: %f2 stn.y: %f2\n",st.x,st.y,st.neighbors[0]->x,st.neighbors[0]->y);
 
-		}else{
-		  	if (L2Distance(&st,&graph[i]) < L2Distance(&st,(st.neighbors[0]))){
+			}else{
+			if (L2Distance(&st,&graph[i]) < L2Distance(&st,(st.neighbors[0]))){
 				st.neighbors[0] = &(graph[i]);
 				graph[i].neighbors[3] = &st;
-		  }
-		  writeDebugStream("st.x: %f2 st.y: %f2 stn.x: %f2 stn.y: %f2\n",st.x,st.y,st.neighbors[0]->x,st.neighbors[0]->y);
+			}
+			writeDebugStream("st.x: %f2 st.y: %f2 stn.x: %f2 stn.y: %f2\n",st.x,st.y,st.neighbors[0]->x,st.neighbors[0]->y);
 
 		}
 
 		if (goal.neighbors[0] == NULL){
 			writeDebugStream("set goal's initial neighbor. \n");
-		  goal.neighbors[0] = &graph[i];
+			goal.neighbors[0] = &graph[i];
 			graph[i].neighbors[4] = &goal;
-		}else{
+			}else{
 			if (L2Distance(&goal,&graph[i]) < L2Distance(&goal,(goal.neighbors[0]))){
 				goal.neighbors[0] = &(graph[i]);
-	  		graph[i].neighbors[4] = &goal;
-		  }
+				graph[i].neighbors[4] = &goal;
+			}
+			writeDebugStream("goal.x: %f2 goal.y: %f2 goal.x: %f2 goal.y: %f2\n",goal.x,goal.y,goal.neighbors[0]->x,goal.neighbors[0]->y);
 		}
-  }
+	}
 
 	writeDebugStream("successfully put in start and goal.\n");
 	//now do dfs to find a path. dfs(start,goal)
 
-	float cs[14];
+	float cs[16];
 	path botPath;
 	botPath.nElems = 0;
 	botPath.coords = cs;
 	botPath.coords[0] = startx;
 	botPath.coords[1] = starty;
 
-  node visitedNodes[14];
-  int yay = 0;
-  yay = DFS(&botPath, &st, &goal, visitedNodes, 0);
+	node visitedNodes[14];
+	int yay = 0;
+	yay = DFS(&botPath, &st, &goal, visitedNodes, 0);
 
 
-  if (!yay) writeDebugStream("DFS unsuccessful! \n");
-  //path[0] = st.x; path[1] = st.y;
-  //path[2] = goal.x; path[3] = goal.y;
+	if (!yay) writeDebugStream("DFS unsuccessful! \n");
+	//path[0] = st.x; path[1] = st.y;
+	//path[2] = goal.x; path[3] = goal.y;
 
 	writeDebugStream("DFS completed.\n");
 
@@ -146,15 +147,16 @@ writeDebugStream("set up neighbors, except for start and goal.\n");
 
 
 
-
-
-
- //botPath = makePath(stx,sty,glx,gly);
+	writeDebugStream("botPath length is: %d\n", botPath.nElems);
+	for (int i = 1; i < botPath.nElems; i++) {
+		writeDebugStream("Path point: %d is x:%f, y:%f\n",i,botPath.coords[2*i], botPath.coords[2 * i + 1]);
+	}
+	//botPath = makePath(stx,sty,glx,gly);
 	writeDebugStream("going!\n");
 
-	for(int i = 0; i < botPath.nElems; i++){
-		writeDebugStream("going to (%f2, %f2)\n", botPath.coords[i], botPath.coords[i+1]);
-	  driveTo(&rob,(botPath.coords[i])*100,(botPath.coords[i+1])*100);
-  }
+	for(int i = 1; i < botPath.nElems; i++){
+		writeDebugStream("going to (%f2, %f2)\n", botPath.coords[2 * i], botPath.coords[2 * i+1]);
+		driveTo(&rob,(botPath.coords[2 * (i)])*100 - 15,(botPath.coords[2 * i + 1])*100 - 15);
+	}
 
 }
